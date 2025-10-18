ea8a70b85f2b8aa5e2cf3f483c99de6bd43b53d2
nathsuki73 <nathsegovia73@gmail.com>
2025-10-18 14:54:32 +0800

    Created button animation

--- src/components/buttons/MenuButton.tsx
+++ src/components/buttons/MenuButton.tsx
@@ -1,34 +1,45 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { gsap } from "gsap";
+import { useGSAP } from '@gsap/react';
 
 const MenuButton = () => {
+  const [isOpen, setOpen] = useState(true)
+  const container = useRef<HTMLDivElement>(null);
+  const tl = useRef<GSAPTimeline>(null)
+
+  useGSAP(() => {
+    tl.current = gsap.timeline({paused: true})
+    .to(".bar1", {
+      duration: 0.2,
+      y: 10,
+      ease: "none"
+    }, 0)
+    .to(".bar2", {
+      duration: 0.2,
+      y: -10,
+      ease: "none"
+    }, 0)
+  }, {scope: container})
+  
+  const handleClick = () => {
+    setOpen(!isOpen)
+    if (isOpen) {
+      tl.current?.play()
+    } else {
+      tl.current?.reverse() 
+    }
+  }
   return (
     <>
-        <div className='hamburger'>
-            <div className='burger burger1'></div>
-            <div className='burger burger2'></div>
+        <div
+        onClick={() => handleClick()} 
+        ref={container}
+        className={`flex flex-col justify-center items-end w-10 gap-2 h-10 overflow-hidden cursor-pointer`}>
+            <span className={`bar1 h-[2px] w-full transition-all bg-black duration-500 ease-in-out`}></span>
+            <span className={`bar2 h-[2px] w-full transition-all bg-black duration-500 ease-in-out`}></span>
         </div>
-
-        <style jsx>{`
-                .hamburger {
-                    display: block;
-                    background-color: red;
-
-                }
-
-                .burger {
-                    display: block;
-                    width: 100%;
-                    height: 2px;
-                    background-color: blue;
-                }
-                .burger1 {
-                
-                }
-                .burger2 {
-                
-                }
-        `}</style>
+        
     </>
   )
 }

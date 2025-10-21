641b159335e659f20ce520dd8ab8314ae20b7b60
nathsuki73 <nathsegovia73@gmail.com>
2025-10-21 23:47:06 +0800

    Created a button link component

---
+++ src/components/buttons/LinkButton.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+
+type LinkButtonProps = {
+    reverse?: boolean
+    val: string
+}
+
+const LinkButton = ({reverse = true, val}: LinkButtonProps) => {
+  return (
+    <Link href="" className={`flex border-2 h-11 justify-center items-center  ${reverse ? "":"bg-foreground text-background"} `}>
+        {val}
+    </Link>
+  )
+}
+
+export default LinkButton

6520100a467785670dd5bb7c4d47f4bf71e8eb39
nathsuki73 <nathsegovia73@gmail.com>
2025-10-21 23:47:38 +0800

    initial landing page for mobile

--- src/components/sections/Landing.tsx
+++ src/components/sections/Landing.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import LinkButton from '../buttons/LinkButton'
+import Image from 'next/image'
 export const Landing = () => {
   return (
     <section className='flex flex-col gap-4 px-6 h-screen py-4'>
@@ -9,7 +10,13 @@ export const Landing = () => {
             <h1 className=''>RATIO</h1>
         </div>
       </div>
+      <div className='relative h-40 overflow-hidden'>
+        <Image className='absolute left-0 -top-38' alt='image1' src={"/image1.png" } width={320} height={200}></Image>
+
+      </div>
       <h1 className=''>We’re more than coffee — we’re a gathering place for friends, creators, and dreamers.</h1>
+      <LinkButton reverse={false} val="Find Us" ></LinkButton>
+      <LinkButton val='More About Us'></LinkButton>
     </section>
   )
 }

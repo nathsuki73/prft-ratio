cb0fd9ff37655d54e7639af534996e4a20a3dbe2
nathsuki73 <nathsegovia73@gmail.com>
2025-10-26 15:20:02 +0800

    Refactor ShowcaseMenu component to include images in descriptions and enhance layout structure

--- src/components/sections/ShowcaseMenu.tsx
+++ src/components/sections/ShowcaseMenu.tsx
@@ -4,13 +4,13 @@ import LinkButton from '../buttons/LinkButton'
 
 export const ShowcaseMenu = () => {
   return (
-    <section className='flex flex-col gap-4 px-6 md:px-12 lg:px-32 2xl:px-110 py-4 bg-[#3C2A21] text-white'>
+    <section className='flex flex-col gap-4 px-6 md:px-12 lg:px-32 2xl:px-110 py-16 bg-[#3C2A21] text-white'>
         <h1 className='text-6xl sm:text-8xl font-bold'>Explore<br />the Menu</h1>
-        <div  className='grid grid-cols-1'>
+        <div  className='flex flex-col gap-8'>
             
-            <Description head='Espresso, Latte & Frappe' body='“From bold espresso shots to creamy lattes and ice-blended frappes — every cup is crafted to energize your day.”'></Description>
-            <Description head='Espresso, Latte & Frappe' body='“From bold espresso shots to creamy lattes and ice-blended frappes — every cup is crafted to energize your day.”'></Description>
-            <Description head='Espresso, Latte & Frappe' body='“From bold espresso shots to creamy lattes and ice-blended frappes — every cup is crafted to energize your day.”'></Description>
+            <Description img='/espresso.png' head='Espresso, Latte & Frappe' body='“From bold espresso shots to creamy lattes and ice-blended frappes — every cup is crafted to energize your day.”'></Description>
+            <Description img='/pasta.png' head='Creamy, Dreamy Pasta' body='“Rich sauces, perfectly cooked noodles, and flavors that warm your soul.”'></Description>
+            <Description img='/burger.png' head='Irresistible Burgers' body='“Juicy, cheesy, and packed with bold flavor — every bite hits just right.”'></Description>
         </div>
     </section>
   )
@@ -18,25 +18,28 @@ export const ShowcaseMenu = () => {
 
 type DescriptionProps = {
     head: string,
-    body: string
+    body: string,
+    img: string
 }
 
-const Description = ({head, body}: DescriptionProps) => {
+const Description = ({head, body, img}: DescriptionProps) => {
     return (
-    <div className='flex flex-col py-4 gap-4'>
-        <h1 className='text-4xl font-bold'>{head}</h1>
-        <div className='relative overflow-hidden h-80'>
+    <div className='flex flex-col sm:flex-row py-4 gap-4'>
+        <div className='relative sm:w-5/12 overflow-hidden h-80'>
             <Image 
                 className='absolute -top-16' 
                 alt='image1' 
-                src={"/espresso.png"} 
+                src={img} 
                 width={320} 
                 height={200} 
                 />
         </div>
-        <h1>{body}</h1>
-        <div className='w-1/2'>
-            <LinkButton val='See Full Menu' reverse={false}></LinkButton>
+        <div className='flex flex-col sm:w-1/2 gap-8  h-80 justify-between'>
+            <h1 className='text-6xl font-bold'>{head}</h1>
+            <h1>{body}</h1>
+            <div className='w-1/2 justify-end'>
+                <LinkButton val='See Full Menu' reverse={false}></LinkButton>
+            </div>
         </div>
     </div>
     )

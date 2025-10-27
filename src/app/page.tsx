77f97b5bd7dba4251f8de6f995204298d9f3ad2c
nathsuki73 <nathsegovia73@gmail.com>
2025-10-27 23:53:23 +0800

    feat: add FindUs component with background image and call-to-action button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Header } from "@/components/Header";
 import { Landing } from "@/components/sections/Landing";
 import { ShowcaseMenu } from "@/components/sections/ShowcaseMenu";
+import { FindUs } from "@/components/sections/FindUs";
 export default function Home() {
   return (
     <div className="">
      <Header></Header>
       <Landing></Landing>
       <ShowcaseMenu></ShowcaseMenu>
+      <FindUs></FindUs>
     </div>
   );
 }
---
+++ src/components/sections/FindUs.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image'
+import LinkButton from '../buttons/LinkButton'
+export const FindUs = () => {
+  return (
+    <div className="relative w-full h-96">
+        <Image
+            src="/coffee-bg.png"
+            alt="coffee background"
+            fill
+            className="object-cover"
+            priority
+        />
+
+        <div className='absolute inset-0'>
+            <div className='flex flex-col p-4 justify-center items-center lg:items-start gap-8 sm:w-4/5 lg:w-3/5 ml-auto text-center lg:text-start'>
+                <h1 className='text-4xl font-bold'>Brewed Near You</h1>
+                <h1 className='w-5/6'>From cozy corners to lively streets, our branches are crafted to be your go-to spot for every coffee craving. Whether you’re after a quiet moment or a quick caffeine fix, there’s a perfect place waiting for you—warm, welcoming, and always brewing something special.</h1>
+                <div className='w-1/2 md:w-1/3 lg:w-1/4 justify-end'>
+                    <LinkButton val='Find Us'></LinkButton>
+                </div>
+            </div>
+        </div>
+</div>
+
+  )
+}
+

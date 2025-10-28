10d6b31c8ccdc35b98033589ca73de78b81c841c
nathsuki73 <nathsegovia73@gmail.com>
2025-10-28 17:04:59 +0800

    feat: add Contact component for user inquiries and feedback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/components/Header";
 import { Landing } from "@/components/sections/Landing";
 import { ShowcaseMenu } from "@/components/sections/ShowcaseMenu";
 import { FindUs } from "@/components/sections/FindUs";
+import { Contact }  from "@/components/sections/Contact";
 export default function Home() {
   return (
     <div className="">
@@ -9,6 +10,7 @@ export default function Home() {
       <Landing></Landing>
       <ShowcaseMenu></ShowcaseMenu>
       <FindUs></FindUs>
+      <Contact></Contact>
     </div>
   );
 }
---
+++ src/components/sections/Contact.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Image from 'next/image'
+import LinkButton from '../buttons/LinkButton'
+export const Contact = () => {
+  return (
+    <section className="relative flex flex-col gap-4 px-6 md:px-12 lg:px-32 2xl:px-110 w-full h-96 py-24">
+        <h1 className='text-6xl sm:text-8xl font-bold'>Contact Us.</h1>
+        <div className='grid grid-cols-2'>
+           <TextField label="Name" />
+           <TextField label="Phone" />
+           <TextField label="Email" />
+           <TextField label="Subject" />
+           <TextField label="Message" className='col-span-2' />
+        </div>
+    </section>
+
+  )
+}
+
+type TextFieldProps = {
+    label: string
+    className?: string
+}
+
+const TextField: React.FC<TextFieldProps> = ({ label, className }) => {
+
+    const inputId = label?.toLowerCase()
+    return (
+        <div className={`flex flex-col items-start gap-2 p-4 ${className}`}>
+            <label htmlFor={`${inputId}`} className="text-sm font-medium text-gray-700">
+                {label}
+            </label>
+            <input
+                id={`${inputId}`}
+                type="text"
+                placeholder="Enter your name"
+                className="w-full border-b-2 border-black p-2 text-gray-900 outline-none transition"
+            />
+    </div>
+    )
+}
+
